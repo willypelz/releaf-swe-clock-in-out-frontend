9f2355323070d7d1da961fe8401c2ff3b9e7175b
willypelz <pelumiasefon@gmail.com>
2021-06-19 15:13:43 +0100

    [IMP] add login screen

--- src/home/login/Login.vue
+++ src/home/login/Login.vue
@@ -1,14 +1,7 @@
 <template>
     <div>
         <!-- Header -->
-        <div class="header bg-gradient-success py-7 py-lg-8 pt-lg-9">
-            <div class="separator separator-bottom separator-skew zindex-100">
-                <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
-                </svg>
-            </div>
-        </div>
+        <div class="header py-7 py-lg-5 pt-lg-9"></div>
 
         <!-- Page content -->
         <b-container class="mt--8 pb-5">
@@ -17,20 +10,21 @@
                     <b-card no-body class="bg-secondary border-0 mb-0">
                         <b-card-body class="px-lg-5 py-lg-5">
                             <div class="text-center text-muted mb-4">
-                                sign in
+                               <h3>Staff portal </h3>
                             </div>
                             <validation-observer v-slot="{handleSubmit}" ref="formValidator">
-                              <b-alert show dismissible variant="danger" v-if="errors">
-                                <span class="alert-text"><strong>Failed!</strong> {{errors.User}}!</span>
+                              <b-alert show dismissible variant="danger" v-if="error">
+                                <span class="alert-text"><strong>Failed! </strong> {{error}}!</span>
                               </b-alert>
+
                                 <b-form role="form" @submit.prevent="handleSubmit(onSubmit)">
                                     <base-input alternative
                                                 class="mb-3"
-                                                name="Email"
-                                                :rules="{required: true, email: true}"
-                                                prepend-icon="ni ni-email-83"
-                                                placeholder="Email"
-                                                v-model="model.email">
+                                                name="Staff ID"
+                                                :rules="{required: true}"
+                                                prepend-icon="ni ni-key-25"
+                                                placeholder="Staff ID"
+                                                v-model="model.staff_id">
                                     </base-input>
 
                                     <base-input alternative
@@ -43,7 +37,6 @@
                                                 v-model="model.password">
                                     </base-input>
 
-                                    <b-form-checkbox v-model="model.rememberMe">Remember me</b-form-checkbox>
                                     <div class="text-center">
                                         <base-button type="primary" native-type="submit" class="my-4">Sign in</base-button>
                                     </div>
