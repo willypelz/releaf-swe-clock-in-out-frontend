fe8b263df98e74f65337ec27f6fb92bf3955e009
willypelz <pelumiasefon@gmail.com>
2021-06-19 15:09:08 +0100

    [IMP] add store index

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,15 @@ import Vue from 'vue';
 /*using the  vuex as the state management which also have object storing ability*/
 import Vuex from 'vuex';
 import user from './modules/user.module';
-import company from './modules/company.module';
-import review from './modules/review.module';
-
+import staff from './modules/staff.module';
+import clockInOut from './modules/clockinout.module';
 
 /*registering vuex application*/
 Vue.use(Vuex);
 const modules = {
   user,
-  company,
-  review
+  staff,
+  clockInOut
 };
 
 /*exporting the vuex store for the application to use */
