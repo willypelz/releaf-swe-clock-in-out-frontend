50c5114d46493c0607810d19af732207a21cf15c
willypelz <pelumiasefon@gmail.com>
2021-06-19 15:18:21 +0100

    [IMP] add actions

--- src/store/actions/actions.type.js
+++ src/store/actions/actions.type.js
@@ -2,16 +2,12 @@
 export const REGISTER_USER = "register_user";
 export const LOGIN_USER = "login_user";
 export const LOGOUT_USER = "logout_user";
-export const GET_COMPANIES = "get_companies";
-export const GET_ACTIVATED_REVIEW = "get_activated_review";
-export const GET_COMPANY = "get_company";
-export const CREATE_COMPANY = "create_company";
-export const DELETE_COMPANY = "delete_company";
+export const GET_STAFFS = "get_staffs";
+export const GET_STAFF = "get_staff";
+export const CREATE_STAFF = "create_staff";
+export const UPDATE_STAFF = "update_staff";
+export const DELETE_STAFF = "delete_staff";
+export const GET_STAFF_CLOCK_IN_OUT_LOGS = "get_staff_clock_in_out_logs";
+export const CLOCK_IN_OUT = "clock_in_out";
 
 
-
-export const GET_REVIEWS = "get_companies";
-export const GET_REVIEW = "get_company";
-export const UPDATE_REVIEW = "update_company";
-export const CREATE_REVIEW = "create_review";
-export const DELETE_REVIEW = "delete_review";
