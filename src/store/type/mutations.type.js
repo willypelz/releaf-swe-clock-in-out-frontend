4a12eac6b5fb4b785dea3207a9e96cc0df1990bc
willypelz <pelumiasefon@gmail.com>
2021-06-19 15:08:19 +0100

    [IMP] add mutations

--- src/store/type/mutations.type.js
+++ src/store/type/mutations.type.js
@@ -1,8 +1,9 @@
-/* Company mutations */
-export const SET_COMPANIES = "set_companies";
-export const SET_COMPANY = "set_company";
+/* Staff mutations */
+export const SET_STAFFS = "set_staffs";
+export const SET_STAFF = "set_staff";
 
 
 /* Users mutations */
 export const SET_USERS = "set_users";
 export const SET_USER = "set_user";
+export const SET_STAFF_LOGS = "set_staff_logs";
